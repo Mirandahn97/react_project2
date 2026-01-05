b6c3812e80bc225d6b13d3d5bb2a51039d8e95df
Mirandahn97 <mira0249@elev.techcollege.dk>
2026-01-05 14:01:38 +0100

    v1

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+
+    return(
+    <>
+        <h1>React 1. project</h1>
+        <article>
+        <img src="" alt="" />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam perspiciatis praesentium temporibus reprehenderit magnam, voluptate consequuntur ratione? Quibusdam quasi quas id quidem soluta, rerum tempora, asperiores ullam necessitatibus doloremque animi!</p>
+        </article>
+    </>
+    )
+}
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,17 @@
+export const Main = () => {
+
+    return (
+        <>
+            <section>
+                <article>
+                    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis voluptatem labore, doloribus deleniti voluptates culpa placeat molestias, dolores explicabo sapiente consequatur? Saepe itaque praesentium, ratione a non fugiat repellat expedita?</p>
+                </article>
+            </section>
+            <section>
+                <article>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima fugiat rerum neque magnam, labore consequatur beatae officiis nobis impedit obcaecati architecto. Inventore totam ipsam tempore iure cumque laudantium! Ut, sit!</p>
+                </article>
+            </section>
+        </>
+    )
+}
